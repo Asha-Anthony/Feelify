08aaad272eec28b92b6d492ac6db670bc6fe2c74
Asha-Anthony <asha.anthony95@gmail.com>
2023-06-07 12:01:18 -0700

    camera is capturing images

--- src/components/camera/camera.js
+++ src/components/camera/camera.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Webcam from "react-webcam";
 import { useRef , useState} from "react";
 export default function Camera() {
 
     const webCamRef = useRef(null);
     const[ imgSrc , setImage ] = useState(null);
+
+    const click = useCallback(()=>{
+        const imgSrc = webCamRef.current.getScreenshot();
+        setImage(imgSrc);
+    },[webCamRef]);
+
+    const reclick =() =>{
+        setImage(null);
+    }
+
   return (
     <div className="container">
-    <Webcam height={600} width={600} ref ={webCamRef} />
+    {imgSrc ? (
+      <img src={imgSrc} alt="webcam" />
+    ) : (
+      <Webcam height={600} width={600} ref={webCamRef} mirrored={true} screenshotFormat="image/jpeg" />
+    )}
+    <div className="btn-container">
+    {imgSrc ? (
+          <button onClick={reclick}>RECLICK !</button>
+        ) : (
+          <button onClick={click}>CLICK!</button>
+        )}
+    </div>
   </div>
   )
 }
