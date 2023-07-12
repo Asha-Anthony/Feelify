65586576e0cea140b982ddb50c10f53cd63f99cd
Asha-Anthony <asha.anthony95@gmail.com>
2023-07-12 13:14:52 -0700

    added library feature

---
+++ src/screens/library/playlist.js
@@ -0,0 +1,137 @@
+
+export const playlist =  [
+  {
+    "name": "Happy",
+    "total":20,
+    "thumbnail": "https://e1.pxfuel.com/desktop-wallpaper/213/149/desktop-wallpaper-sunset-spotify-playlist-cover-spotify-covers-thumbnail.jpg",
+    "tracks": [
+      {
+        "title": "Track 1",
+        "artist": "Artist 1",
+        "thumbnail": "https://example.com/track1_thumbnail.jpg"
+      },
+      {
+        "title": "Track 2",
+        "artist": "Artist 2",
+        "thumbnail": "https://example.com/track2_thumbnail.jpg"
+      },
+      {
+        "title": "Track 3",
+        "artist": "Artist 3",
+        "thumbnail": "https://example.com/track3_thumbnail.jpg"
+      }
+    ]
+  },
+  {
+    "name": "Sad",
+    "total":40,
+    "thumbnail": "https://i.pinimg.com/236x/f5/db/5f/f5db5fbdb2d24bc83d8bebabbb6c4540.jpg",
+    "tracks": [
+      {
+        "title": "Track 4",
+        "artist": "Artist 4",
+        "thumbnail": "https://example.com/track4_thumbnail.jpg"
+      },
+      {
+        "title": "Track 5",
+        "artist": "Artist 5",
+        "thumbnail": "https://example.com/track5_thumbnail.jpg"
+      },
+      {
+        "title": "Track 6",
+        "artist": "Artist 6",
+        "thumbnail": "https://example.com/track6_thumbnail.jpg"
+      }
+    ]
+  },
+  {
+    "name": "Energetic",
+    "total":10,
+    "thumbnail": "https://e1.pxfuel.com/desktop-wallpaper/22/826/desktop-wallpaper-aesthetics-vibes-playlist-cover-playlist-covers-thumbnail.jpg",
+    "tracks": [
+      {
+        "title": "Track 7",
+        "artist": "Artist 7",
+        "thumbnail": "https://example.com/track7_thumbnail.jpg"
+      },
+      {
+        "title": "Track 8",
+        "artist": "Artist 8",
+        "thumbnail": "https://example.com/track8_thumbnail.jpg"
+      },
+      {
+        "title": "Track 9",
+        "artist": "Artist 9",
+        "thumbnail": "https://example.com/track9_thumbnail.jpg"
+      }
+    ]
+  },
+  {
+    "name": "Relaxed",
+    "total":29,
+    "thumbnail": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWD1Yryy1Jfy8RemgmQEbglv8Ows6J1ifs3g&usqp=CAU",
+    "tracks": [
+      {
+        "title": "Track 10",
+        "artist": "Artist 10",
+        "thumbnail": "https://example.com/track10_thumbnail.jpg"
+      },
+      {
+        "title": "Track 11",
+        "artist": "Artist 11",
+        "thumbnail": "https://example.com/track11_thumbnail.jpg"
+      },
+      {
+        "title": "Track 12",
+        "artist": "Artist 12",
+        "thumbnail": "https://example.com/track12_thumbnail.jpg"
+      }
+    ]
+  },
+  {
+    "name": "Upbeat",
+    "total":5,
+    "thumbnail": "https://www.musicinminnesota.com/wp-content/uploads/2022/01/Photo-from-Adobe-Stock.jpg",
+    "tracks": [
+      {
+        "title": "Track 13",
+        "artist": "Artist 13",
+        "thumbnail": "https://example.com/track13_thumbnail.jpg"
+      },
+      {
+        "title": "Track 14",
+        "artist": "Artist 14",
+        "thumbnail": "https://example.com/track14_thumbnail.jpg"
+      },
+      {
+        "title": "Track 15",
+        "artist": "Artist 15",
+        "thumbnail": "https://example.com/track15_thumbnail.jpg"
+      }
+    ]
+  },
+  {
+    "name": "Chill",
+    "total":24,
+    "thumbnail": "https://i.pinimg.com/736x/3a/06/a9/3a06a9236b8ef1265a7fa0181c32bc68.jpg",
+    "tracks": [
+      {
+        "title": "Track 16",
+        "artist": "Artist 16",
+        "thumbnail": "https://example.com/track16_thumbnail.jpg"
+      },
+      {
+        "title": "Track 17",
+        "artist": "Artist 17",
+        "thumbnail": "https://example.com/track17_thumbnail.jpg"
+      },
+      {
+        "title": "Track 18",
+        "artist": "Artist 18",
+        "thumbnail": "https://example.com/track18_thumbnail.jpg"
+      }
+    ]
+  }
+]
+  
+  
